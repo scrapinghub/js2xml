437c8b59148ccb31ac7480ab6c9e9784e2dd6295
Paul Tremberth <paul.tremberth@gmail.com>
2014-05-02 12:14:50 +0200

    Add js2xml.parse() method that wraps the slimit visitor/xml-builder

--- js2xml/__init__.py
+++ js2xml/__init__.py
@@ -1,1 +1,11 @@
+import lxml.etree
+from slimit.parser import Parser
+from js2xml.xmlvisitor import XmlVisitor
 
+_parser = Parser()
+_visitor = XmlVisitor()
+
+def parse(text, debug=False):
+    tree = _parser.parse(text, debug=debug)
+    xml = _visitor.visit(tree)
+    return xml
