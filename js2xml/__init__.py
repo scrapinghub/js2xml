967ab2d8da794ee8c9302b6268466f984c98f6c7
Paul Tremberth <paul.tremberth@gmail.com>
2014-05-02 21:44:20 +0200

    Extract JSON-like data as dicts
    Add pretty-printer helper

--- js2xml/__init__.py
+++ js2xml/__init__.py
@@ -9,3 +9,60 @@ def parse(text, debug=False):
     tree = _parser.parse(text, debug=debug)
     xml = _visitor.visit(tree)
     return xml
+
+
+def pretty_print(tree):
+    return lxml.etree.tostring(tree, pretty_print=True)
+
+
+_jsonlike_xpath = """
+    (self::object or self::array)
+    and
+    count(./descendant-or-self::*[not(   self::object
+                                      or self::array
+                                      or self::assign
+                                      or self::left
+                                      or self::right
+                                      or self::operator[.=":"]
+                                      or self::string
+                                      or self::number
+                                      or self::boolean)])=0
+"""
+_xp_jsonlike = lxml.etree.XPath(_jsonlike_xpath)
+def is_jsonlike(subtree):
+    return _xp_jsonlike(subtree)
+
+_findjson_xpath = """
+    .//*[self::object or self::array]
+        [count(.//*[not(   self::object
+                        or self::array
+                        or self::assign
+                        or self::left
+                        or self::right
+                        or self::operator[.=":"]
+                        or self::string
+                        or self::number
+                        or self::boolean)])=0]
+"""
+_xp_findjsonlike = lxml.etree.XPath(_findjson_xpath)
+def find_jsonlike(tree):
+    return _xp_findjsonlike(tree)
+
+
+def make_dict(tree):
+    if tree.tag == 'array':
+        return [make_dict(child) for child in tree.iterchildren()]
+    elif tree.tag == 'object':
+        return dict([make_dict(child) for child in tree.iterchildren()])
+    elif tree.tag == 'assign':
+        return (make_dict(tree.find('./left/*')), make_dict(tree.find('./right/*')))
+    elif tree.tag == 'string':
+        return tree.text
+    elif tree.tag == 'boolean':
+        return tree.text == 'true'
+    elif tree.tag == 'number':
+        try:
+            return int(tree.text)
+        except:
+            return float(tree.text)
+
