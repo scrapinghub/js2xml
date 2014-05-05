5255c334478b1370b84cf661c9c4b9b117a70896
Paul Tremberth <paul.tremberth@gmail.com>
2014-05-05 15:22:14 +0200

    Cleanup strings
    Add tests

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -42,3 +42,78 @@ def test_parse_exception():
 
     for snippet in jscode_snippets:
         assert_raises(SyntaxError, js2xml.parse, snippet)
+
+
+def test_parse_string():
+    jscode_snippets = [
+        (
+        r"""
+        var i = 'test';
+        """, [r'test']
+        ),
+        (
+        r"""
+        var i = 'test\'s output';
+        """, [r"test's output"]
+        ),
+        (
+        r"""
+        var i = "test";
+        """, [r'test']
+        ),
+        (
+        r"""
+        var i = "test\'s output";
+        """, [r"test\'s output"]
+        ),
+        (
+        r"""
+        var i = "nested \"quotes\".";
+        """, [r'nested "quotes".']
+        ),
+    ]
+
+    for snippet, expected in jscode_snippets:
+        jsxml = js2xml.parse(snippet)
+        result = jsxml.xpath("//string/text()")
+        assert_list_equal(result, expected)
+
+
+def test_parse_url():
+    jscode_snippets = [
+        (
+        r"""
+        var i = 'http://www.example.com';
+        """, [r'http://www.example.com']
+        ),
+        (
+        r"""
+        var i = 'http:\/\/www.example.com';
+        """, [r"http://www.example.com"]
+        ),
+    ]
+
+    for snippet, expected in jscode_snippets:
+        jsxml = js2xml.parse(snippet)
+        result = jsxml.xpath("//string/text()")
+        assert_list_equal(result, expected)
+
+
+def test_parse_number():
+    jscode_snippets = [
+        (
+        r"""
+        var i = 3;
+        """, [r'3']
+        ),
+        (
+        r"""
+        var i = -3.14;
+        """, [r"-3.14"]
+        ),
+    ]
+
+    for snippet, expected in jscode_snippets:
+        jsxml = js2xml.parse(snippet)
+        result = jsxml.xpath("//number/text()")
+        assert_list_equal(result, expected)
