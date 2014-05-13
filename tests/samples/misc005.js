38c965fadc71dc93dab6c1cee6615c362dc06203
Paul Tremberth <paul.tremberth@gmail.com>
2014-05-14 00:03:06 +0200

    Move sample snippets location to tests/

---
+++ tests/samples/misc005.js
@@ -0,0 +1,9 @@
+gapi.load("gapi.iframes:gapi.iframes.style.bubble", function() {
+          if (gapi.iframes && gapi.iframes.getContext) {
+            gapi.iframes.getContext().openChild({
+                url: 'https://www.blogger.com/navbar.g?targetBlogID\0754325487278375417853\46blogName\75spirello\46publishMode\75PUBLISH_MODE_BLOGSPOT\46navbarType\75LIGHT\46layoutType\75LAYOUTS\46searchRoot\75http://spirelloskrimskramserier.blogspot.com/search\46blogLocale\75no\46v\0752\46homepageUrl\75http://spirelloskrimskramserier.blogspot.com/\46vt\0751357383140196484672',
+                where: document.getElementById("navbar-iframe-container"),
+                id: "navbar-iframe"
+            });
+          }
+        });
---
+++ tests/samples/quotes001.js
@@ -0,0 +1,2 @@
+var test1 = '124234' + "asdasf" + '(\'events\',\'event4\')';
+var test2 = "124234" + 'asdasf' + "(\"events\",\"event4\")";
