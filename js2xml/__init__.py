44383739a0eb82ea2b53bd9a118054107c841739
Paul Tremberth <paul.tremberth@gmail.com>
2016-06-10 16:16:50 +0200

    Bump version: 0.2.0 → 0.2.1

--- js2xml/__init__.py
+++ js2xml/__init__.py
@@ -6,7 +6,7 @@ from .xmlvisitor import XmlVisitor
 from .jsonlike import *
 
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 _parser = Parser()
 _visitor = XmlVisitor()
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='js2xml',
-      version='0.2.0',
+      version='0.2.1',
       description='Convert Javascript code to XML document',
       long_description="""
 ======
@@ -38,5 +38,5 @@ bits in Javascript instructions (strings, IDs, function parameters...)
         'Intended Audience :: Developers',
      ],
      url = 'https://github.com/redapple/js2xml',
-     download_url = 'https://github.com/redapple/js2xml/archive/v0.2.0.tar.gz',
+     download_url = 'https://github.com/redapple/js2xml/archive/v0.2.1.tar.gz',
 )
