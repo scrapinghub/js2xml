c58e197304ca0fe6ed673631acd1f50a8360dba2
Paul Tremberth <paul.tremberth@gmail.com>
2017-05-30 12:05:48 +0200

    Set level for logger too

--- js2xml/log.py
+++ js2xml/log.py
@@ -2,6 +2,7 @@ import logging
 
 # basic example from https://docs.python.org/2/howto/logging.html#configuring-logging
 logger = logging.getLogger('js2xml')
+logger.setLevel(logging.ERROR)
 handler = logging.StreamHandler()
 handler.setLevel(logging.ERROR)
 formatter = logging.Formatter('[%(name)s] %(levelname)s: %(message)s')
