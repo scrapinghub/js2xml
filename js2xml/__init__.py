c492552da9d34221d88935fffdf1777793dadb77
Adrián Chaves <adrian@chaves.io>
2020-06-04 14:13:27 +0200

    Bump version: 0.3.1 → 0.4.0

--- js2xml/__init__.py
+++ js2xml/__init__.py
@@ -6,7 +6,7 @@ from .jsonlike import *
 from .xmlvisitor import XmlVisitor
 
 
-__version__ = "0.3.1"
+__version__ = "0.4.0"
 
 _parser = Parser()
 _visitor = XmlVisitor()
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='js2xml',
-      version='0.3.1',
+      version='0.4.0',
       description='Convert Javascript code to XML document',
       long_description="""
 ======
@@ -40,5 +40,5 @@ bits in Javascript instructions (strings, IDs, function parameters...)
         'Intended Audience :: Developers',
      ],
      url = 'https://github.com/scrapinghub/js2xml',
-     download_url = 'https://github.com/scrapinghub/js2xml/archive/v0.3.1.tar.gz',
+     download_url = 'https://github.com/scrapinghub/js2xml/archive/v0.4.0.tar.gz',
 )
